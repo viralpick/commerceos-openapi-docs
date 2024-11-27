9945f1292ebdce8a8ea4862540f4bba647c89460
enhans-yunsang <yunsang@enhans.ai>
2024-11-28 00:47:03 +0900

    chore: Deploy Github Pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,17 @@ import nextra from 'nextra'
 const withNextra = nextra({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx',
+})
+
+export default {
+    ...withNextra(),
     reactStrictMode: true,
     output: 'export',
     basePath: '/commerceos-openapi-docs',
-})
-
-export default withNextra()
+    images: {
+        unoptimized: true
+    }
+}
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // export default withNextra({ /* other next.js config */ })
