ef190a8e0bbd46aedd5f1612c176c0b15244975e
enhans-yunsang <yunsang@enhans.ai>
2026-09-05 20:43:40 +0900

    feat: Data Connector Tunnel 문서 작성

--- content/pipeline/_meta.js
+++ content/pipeline/_meta.js
@@ -1,5 +1,6 @@
 export default {
   index: "Overview",
+  "data-connector": "Data Connector",
   "flow-nodes": "Flow Nodes",
   "extract-nodes": "Extract Nodes",
   "transform-nodes": "Transform Nodes",
---
+++ content/pipeline/data-connector/_meta.js
@@ -0,0 +1,4 @@
+export default {
+  index: "Data Connector",
+  "tunnel-agent": "터널 Agent 설치 (Docker)",
+};
